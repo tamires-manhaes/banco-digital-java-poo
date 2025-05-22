d01b00b19656eb2cf979f5843bda8f99768707b0
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-05-22 12:27:51 -0300

    Add account management methods to Banco class
    Introduced a constructor to initialize Banco with a name and an empty account list. Added methods to add individual accounts and list all accounts with their details. Removed unnecessary setters for better encapsulation.

--- src/Banco.java
+++ src/Banco.java
@@ -1,22 +1,36 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
   private String nome;
   private List<Conta> contas;
 
-  public String getNome() {
-    return nome;
+  public Banco(String nome){
+    this.nome = nome;
+    this.contas = new ArrayList<>();
+
   }
 
-  public void setNome(String nome) {
-    this.nome = nome;
+  public String getNome() {
+    return nome;
   }
 
   public List<Conta> getContas() {
     return contas;
   }
 
-  public void setContas(List<Conta> contas) {
-    this.contas = contas;
+  public void addConta(Conta conta){
+    this.contas.add(conta);
+  }
+
+  public void listarContas(){
+    this.contas.forEach(conta -> {
+      System.out.println("========== Conta ==========");
+      System.out.println(String.format("Titular: %s",conta.getCliente().getNome()));
+      System.out.println(String.format("Numero: %s",conta.getNumero()));
+      System.out.println(String.format("Agencia: %s",conta.getAgencia()));
+      System.out.println(String.format("Saldo: %.2f",conta.getSaldo()));
+
+    });
   }
 }
