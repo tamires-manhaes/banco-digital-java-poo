774942b4eecb147d02ce15db0414e54b4745bf80
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-05-22 12:28:05 -0300

    Add Conta attribute and methods to Cliente class
    Introduced a new Conta attribute to the Cliente class, along with a constructor for initialization. Also added the criarConta method to associate a Conta object with a Cliente instance.

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,6 +2,13 @@ public class Cliente {
   private String nome;
   private String cpf;
   private String email;
+  private Conta conta;
+
+  public Cliente(String nome, String cpf, String email) {
+     this.nome = nome;
+     this.cpf = cpf;
+     this.email = email;
+  }
 
   public String getNome() {
     return nome;
@@ -9,5 +16,9 @@ public class Cliente {
 
   public void setNome(String nome) {
     this.nome = nome;
+  }
+
+  protected void criarConta(Cliente cliente, Conta conta) {
+    this.conta = conta;
   }
 }
